6357831291295a5d14878e37fd6248c801655ee2
Luke <lukefowlie64@gmail.com>
2024-07-22 00:15:42 +0100

    Formatting Files

--- frontend/public/tuner.js
+++ frontend/public/tuner.js
@@ -1,15 +1,11 @@
 class pitchDetector {
-    constructor() {
-        this.analyser = null;
-        this.audioCaptured = false;
-        this.dataArray = null;
-    }
+  constructor() {
+    this.analyser = null;
+    this.audioCaptured = false;
+    this.dataArray = null;
+  }
 
-    startCapture(){
+  startCapture() {}
 
-    }
-
-    processAudio(){
-
-    }
+  processAudio() {}
 }
